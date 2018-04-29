d1576f19ca0cb2d2e7c4c85fa3a38d1791fec50c
wenfengzCN <35298578+wenfengzCN@users.noreply.github.com>
2018-04-29 23:33:55 +0800

    Delete tech_test.py

--- tech_test.py
+++
@@ -1,6 +0,0 @@
-import logging
-import threading
-import json
-import pandas as pd
-file = pd.read_csv('/home/wenfeng/vlis/testset/jetty1710_test.csv',encoding='ISO-8859-1').assign(pmd=None)
-file.to_csv('/home/wenfeng/jetty_test.csv',index=False)
